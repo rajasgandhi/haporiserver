aeced02b33a892766e7fc43dc84332f800a7d30e
Rajas Gandhi <rajasmgandhi@gmail.com>
2020-12-14 19:55:56 -0600

    update

--- app.js
+++ app.js
@@ -35,4 +35,4 @@ app.post("/sendpost", function (request, response) {
   response.send("Posts updated!");
 });
 
-app.listen(3000, '192.168.1.18' || 'localhost', () => console.log(`app listening on port 3000!`));
+app.listen(process.env.PORT || 3000, () => console.log(`app listening on port ${PORT}!`));
