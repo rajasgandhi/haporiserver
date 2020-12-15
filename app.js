189bca6b0e8736ad734fc74a0f5f7d153152c4f1
Rajas Gandhi <rajasmgandhi@gmail.com>
2020-12-14 19:49:10 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var session = require("express-session");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+
+let posts = []
+
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", function (request, response) {
+  response.send("Please use /getposts or /sendpost!");
+});
+
+app.get("/getposts", function (request, response) {
+  response.send(posts);
+  response.end();
+});
+
+app.post("/sendpost", function (request, response) {
+  const post = request.body;
+  console.log(post);
+  posts.unshift(post);
+  Array.prototype.reverse(posts);
+  response.send("Posts updated!");
+});
+
+app.listen(3000, '192.168.1.18' || 'localhost', () => console.log(`app listening on port 3000!`));
