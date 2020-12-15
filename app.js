d41fef58b02ff3d279ae8b8a4629c1c34474bc8f
Rajas Gandhi <rajasmgandhi@gmail.com>
2020-12-14 20:03:02 -0600

    update server

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var path = require("path");
 var app = express();
 
 var PORT = process.env.PORT || 3000;
-var host = "";
 
 let posts = []
 
@@ -38,9 +37,4 @@ app.post("/sendpost", function (request, response) {
   response.send("Posts updated!");
 });
 
-if(process.env.NODE_ENV.toLowerCase() == 'development') {
-  host = "192.168.1.18";
-} else {
-  host = "haporiserver.herokuapp.com";
-}
-app.listen(PORT, host, () => console.log(`app listening on port ${PORT}!`));
+app.listen(PORT, () => console.log(`app listening on port ${PORT}!`));
