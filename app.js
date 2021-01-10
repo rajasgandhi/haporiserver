a45c6db7401f5f8867d4f40198fe9e7d2e043120
Rajas Gandhi <rajasmgandhi@gmail.com>
2021-01-10 17:52:50 -0600

    update

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var session = require("express-session");
 var bodyParser = require("body-parser");
+var http = require('http');
 
 var app = express();
 
@@ -34,4 +35,27 @@ app.post("/sendpost",  (req, res) => {
   res.sendStatus(200);
 });
 
+function startKeepAlive() {
+  setInterval(function() {
+      var options = {
+          host: 'haporiserver.herokuapp.com',
+          port: 80,
+          path: '/'
+      };
+      http.get(options, function(res) {
+          res.on('data', function(chunk) {
+              try {
+                  // optional logging... disable after it's working
+                  console.log("HEROKU RESPONSE: " + chunk);
+              } catch (err) {
+                  console.log(err.message);
+              }
+          });
+      }).on('error', function(err) {
+          console.log("Error: " + err.message);
+      });
+  }, 20 * 60 * 1000); // load every 20 minutes
+}
+
 app.listen(PORT, () => console.log(`app listening on port ${PORT}!`));
+startKeepAlive();
